9c143d0f072c1d1d9170aa82d253323690d51825
Kirill_Kisel2 <kirill_kisel2@epam.com>
2023-03-02 23:44:24 +0300

    create app and configured a project

---
+++ wg-test/webpack.config.js
@@ -0,0 +1,73 @@
+const path = require ('path');
+const webpack = require ('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+    mode: isProd ? "production" : "development",
+    devtool: isProd ? "source-map" : "eval",
+
+    entry: "./src/index.tsx",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            //
+            // Loading styles
+            //
+            {
+                test: /\.css$/,
+                use: [ isProd ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"]
+            },
+            //
+            // Loading images
+            //
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: "asset/resource",
+            },
+            //
+            // Loading fonts
+            //
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: "asset/inline",
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "WG-test",
+            template: path.resolve(__dirname, "public/index.html"),
+            filename: "index.html",
+            base: '/'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new FaviconsWebpackPlugin('public/favicon.ico')
+    ],
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        open: true,
+        compress: true,
+        port: 8000,
+        historyApiFallback: true,
+        hot: true,
+    },
+};
